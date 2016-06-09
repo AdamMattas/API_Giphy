7fb9183013c543e8ae74fa4008ee1c145652e5ce
adam fader <adamfader@gmail.com>
2016-06-09 19:20:55 -0400

    added initial files

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+//initialized global variables
+
+
+// wrapped jquery in document ready function
+$(document).on('ready', function(){
+
+});
+
+//on button click grab gifs from api and display 10
+
+//capture data from text field
+
+//create new button with data from text field
